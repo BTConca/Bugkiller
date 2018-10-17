996464bb1665a088b73c0dd80bcae7869a32f6e6
BTConca <1512250@student.fit.hcmus.vn>
2018-10-17 10:30:06 +0700

    New update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
-import {Route, withRouter} from 'react-router-dom';
-import auth0Client from './Auth';
-import NavBar from './NavBar/NavBar';
-import Question from './Question/Question';
-import Questions from './Questions/Questions';
-import Callback from './Callback';
-import NewQuestion from './NewQuestion/NewQuestion';
-import SecuredRoute from './SecuredRoute/SecuredRoute';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+
+injectTapEventPlugin();
+
+import './App.css';
+import LoginScreen from './Loginscreen';
 class App extends Component {
-  async componentDidMount() {
-    if (this.props.location.pathname === '/callback') return;
-    try {
-      await auth0Client.silentAuth();
-      this.forceUpdate();
-    } catch (err) {
-      if (err.error === 'login_required') return;
-      console.log(err.error);
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
     }
   }
-
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<LoginScreen appContext={this}/>);
+    this.setState({
+                  loginPage:loginPage
+                    })
+  }
   render() {
     return (
-      <div>
-        <NavBar/>
-        <Route exact path='/' component={Questions}/>
-        <Route exact path='/question/:questionId' component={Question}/>
-        <Route exact path='/callback' component={Callback}/>
-        <SecuredRoute path='/new-question' component={NewQuestion} />
+      <div className="App">
+        {this.state.loginPage}
+        {this.state.uploadScreen}
       </div>
     );
   }
 }
 
-export default withRouter(App);
+export default App;
